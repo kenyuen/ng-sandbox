a42d60d63eb2f34e7f59b521c84f89757708ba64
Kenneth Yuen <kenneth.x.yuen@gmail.com>
2016-11-13 16:39:18 -0600

    Added crypto to service to encrypt payload and sign.

--- ps_jwt/api/api.js
+++ ps_jwt/api/api.js
@@ -2,35 +2,48 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var User = require('./models/User.js');
+var jwt = require('./services/jwt.js');
 
 var app = express();
 
 app.use(bodyParser.json());
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    
+
     next();
 });
 
-app.post('/register', function(req, res){
+app.post('/register', function (req, res) {
     var user = req.body;
-    
+
     var newUser = new User.model({
         email: user.email,
         password: user.password
     });
-    
-    newUser.save(function(err) {
-        res.status(200).json(newUser);
+
+    var payload = {
+        iss: req.hostname,
+        sub: user._id
+    }
+
+    var token = jwt.encode(payload, "shhh");
+
+    newUser.save(function (err) {
+        res.status(200).send({
+            user: newUser.toJSON(),
+            token: token
+        });
     });
 
 });
 
 mongoose.connect('mongodb://localhost/psjwt');
 
-var server = app.listen(3000, function() {
+//console.log(jwt.encode('hi','secret'));
+
+var server = app.listen(3000, function () {
     console.log('api listening on ', server.address().port);
 });
